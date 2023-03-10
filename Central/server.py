ada6cb45bf4a39650351c5ffdaac0e12e496d7a6
pintti <allu.tuovinen@gmail.com>
2023-03-11 00:35:50 +0200

    server search and dronwe functions

--- Central/server.py
+++ Central/server.py
@@ -1,5 +1,8 @@
 import flask
 import numpy as np
+import math
+import heapq
+
 from typing import List
 
 
@@ -7,9 +10,12 @@ processor_dict = {}
 
 
 def create_area():
+    """Creates an area for the new processor
+    Returns:
+        matrix: area matrix that has obstacles"""
     matrix = np.zeros([4,50,50])
     no_go_areas = []
-    while len(no_go_areas) < 6:
+    while len(no_go_areas) < 5:
         rand = np.random.randint(0, 50, size=2)
         add_flag = 1
         if no_go_areas:
@@ -21,17 +27,22 @@ def create_area():
             add_flag = 1
         else:
             no_go_areas.append(rand)
-    spawn_point = no_go_areas.pop(-1)
     matrix = create_obstacles(matrix, no_go_areas)
-    return matrix, spawn_point
+    return matrix
 
 
 def create_obstacles(matrix, obstacle_list) -> List[List[List[int]]]:
+    """Creates obstacles for the area.
+    Args:
+        matrix: area matrix
+        obstacle_list: a list of the obstacles to be placed in a 3x3 area
+    Returns:
+        matrix: area matrix with obstacles"""
     for obstacle in obstacle_list:
         row, col = obstacle
         for height in range(0, 4):
-            for i in range(row-1, row+2):
-                for j in range(col-1, col+2):
+            for i in range(row, row+1):
+                for j in range(col, col+1):
                     if i >= 0 and j >= 0 and i < 50 and j < 50:
                         matrix[height, i, j] = -1
     return matrix
@@ -59,29 +70,86 @@ def connect_processor():
         pass
     else:
         create_demo_processor()
+        process_processor(processor_dict[0])
 
 
-def process_processors():
-    pass
-    
+def process_processor(processor):
+    for drone_num in processor['drones']:
+        if not processor['drones'][drone_num]['moving']:
+            get_drone_steps(processor['drones'][drone_num], processor['area'])
+
 
 def create_demo_processor():
+    """Creates a demo processor to be used for demoing purposes."""
     print("Demo mode")
-    area, spawn_point = create_area()
+    area = create_area()
     processor_dict[0] = {
         'area': area,
-        'spawn': spawn_point,
+        'spawn': get_rand_point(area),
         'drones': {
-            0: create_demo_drone(spawn_point)
+            0: {
+                'location': get_rand_point(area),
+                'goal': get_rand_point(area),
+                'moving': False,
+                'steps': []
+            }
         }
     }
-    print(processor_dict)
+    processor_dict[0]['spawn'][0] = 0 # sets spawn to ground point
 
 
-def create_demo_drone(spawn_point: List[int]):
-    location = [0, spawn_point[0], spawn_point[1]]
-    goal = [0, 0, 0]
-    return {"location": location, 'goal': goal}
+def get_rand_point(area):
+    """Randomize a goal position for a drone."""
+    while True:
+        rand_row, rand_col = np.random.randint(0, 50, size=2)
+        rand_height = np.random.randint(0, 4)
+        if area[rand_height, rand_row, rand_col] == 0:
+            return [rand_height, rand_row, rand_col]
+
+
+def get_drone_steps(drone_dict, area):
+    drone_dict['moving'] = True
+    loc = drone_dict['location']
+    goal = drone_dict['goal']
+    steps = astar(area, tuple(loc), tuple(goal))
+    for i, step in enumerate(steps):
+        steps[i] = np.array(step)
+    drone_dict['steps'] = steps
+    print(drone_dict)
+    # send steps through HTTP
+    
+
+def heuristic(node, goal):
+    return ((node[0] - goal[0])**2 + (node[1] - goal[1])**2 + (node[2] - goal[2])**2)**0.5
+
+
+def astar(matrix, start, goal):
+    movements = [(0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0), (-1, 0, 0), (1, 0, 0)]
+    heap = []
+    old = []
+    heapq.heappush(heap, (heuristic(start, goal), start))
+    costs = {start: 0}
+    parents = {start: None}
+    
+    while heap:
+        current = heapq.heappop(heap)[1]
+        old.append(current)
+        if current == goal:
+            path = []
+            while current:
+                path.append(current)
+                current = parents[current]
+            return path[::-1]
+        for move in movements:
+            neighbor = (current[0] + move[0], current[1] + move[1], current[2] + move[2])
+            if 0 <= neighbor[0] < len(matrix) and 0 <= neighbor[1] < len(matrix[0]) and 0 <= neighbor[2] < len(matrix[0][0]):
+                cost = costs[current] + matrix[neighbor[0]][neighbor[1]][neighbor[2]]
+                if (neighbor not in costs or cost < costs[neighbor]) and neighbor not in old:
+                    costs[neighbor] = cost
+                    priority = cost + heuristic(neighbor, goal)
+                    parents[neighbor] = current
+                    heapq.heappush(heap, (priority, neighbor))
+    return None
 
 
 if __name__ == "__main__":
